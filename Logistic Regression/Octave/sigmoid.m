44c298b5484dc4f9b1dc79b7863a8ee05e96b21a
Victor Gafiatulin <gafiatulin@gmail.com>
2011-10-29 02:34:42 +0400

    Sigmoid in progress.

--- Logistic Regression/Octave/sigmoid.m
+++ Logistic Regression/Octave/sigmoid.m
@@ -8,10 +8,11 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
+function r = for_each(x);
+	r = 1/(1+e^(-x));
+end
+sigm = @for_each;
+g = arrayfun(sigm, z);
 
 % =============================================================
 
