52765124dd2055b9c66feaf166cfc4d1567ff8b8
Victor Gafiatulin <gafiatulin@gmail.com>
2011-11-06 02:32:01 +0400

    Randomly initialize weights

--- Neural network learning/Octave/randInitializeWeights.m
+++ Neural network learning/Octave/randInitializeWeights.m
@@ -19,12 +19,8 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first row of W corresponds to the parameters for the bias units
 %
 
-
-
-
-
-
-
+epsilon_init = sqrt(6)/sqrt(L_in+L_out);
+W = rand(L_out, 1+L_in)*2*epsilon_init-epsilon_init;
 
 
 % =========================================================================
